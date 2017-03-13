b299ca1e9126f7c187da5ecd04926557f7dfdb4c
rufer7 <marc.rufer@d-fens.net>
2017-03-13 17:06:14 +0100

    Activities
    * Sleeper activity implemented
    * RectangleActivity added

---
+++ src/Net.Appclusive.Workflows/Sleeper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using System.Threading.Tasks;
+
+namespace Net.Appclusive.Workflows
+{
+
+    public sealed class Sleeper : CodeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<int> Seconds { get; set; }
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtain the runtime value of the Text input argument
+            var seconds = context.GetValue(this.Seconds);
+            //System.Threading.Thread.Sleep(seconds * 1000);
+            Task.Delay(seconds * 1000);
+        }
+    }
+}
